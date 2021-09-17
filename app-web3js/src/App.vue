e6a6bcb094cb8854b72a51d77e57204d5b799582
Amanda Gonsalves <amandagonsalves75@gmail.com>
2021-09-17 19:35:47 -0300

    add component

--- app-web3js/src/App.vue
+++ app-web3js/src/App.vue
@@ -1,17 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <button @click="increment">increment</button>
+    <button @click="getCount">console count</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import artifact from '../../smart-contracts/build/contracts/Counter.json';
+import Web3 from 'web3';
+import { onMounted, reactive } from "vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  setup() {
+    const state = reactive({
+      contract: null,
+    });
+
+    onMounted(async () => {
+      const web3 = new Web3("http://127.0.0.1:7545");
+      const accounts = await web3.eth.getAccounts();
+      const networkId = await web3.eth.net.getId();
+      const contractAddress = artifact.networks[networkId].address;
+
+      // // instantiate contract instance and assign to component ref variable
+      state.contract = new web3.eth.Contract(artifact.abi, contractAddress, {
+        from: accounts[0],
+      });
+
+      console.log('web3 data', accounts, networkId, contractAddress, state.contract)
+    });
+
+    const increment = async () => {
+      await state.contract.methods.increment().send();
+      console.log('current counter', await state.contract.methods.getCount().call())
+    }
+
+    const getCount = async () => {
+      console.log(await state.contract.methods.getCount().call());
+    }
+
+    return {
+      state,
+      increment,
+      getCount
+    }
+  },
+};
 </script>
 
 <style>
