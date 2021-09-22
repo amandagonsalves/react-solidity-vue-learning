5bbcd4b65d277f60cd15e9ae3afc28154cb66eb1
Amanda Gonsalves <amandagonsalves75@gmail.com>
2021-09-21 21:50:51 -0300

    update escrow contract

--- app-web3js-vue3/src/App.vue
+++ app-web3js-vue3/src/App.vue
@@ -1,50 +1,44 @@
 <template>
   <div>
     <button @click="increment">increment</button>
-    <button @click="getCount">console count</button>
+    <p>{{ state.counter }}</p>
   </div>
 </template>
 
 <script>
 import artifact from '../../smart-contracts/build/contracts/Counter.json';
 import Web3 from 'web3';
-import { onMounted, reactive } from "vue";
+import { onMounted, reactive } from 'vue';
 
 export default {
-  name: "App",
+  name: 'App',
   components: {},
   setup() {
     const state = reactive({
       contract: null,
+      counter: null,
     });
 
     onMounted(async () => {
-      const web3 = new Web3("http://127.0.0.1:7545");
+      const web3 = new Web3('http://127.0.0.1:7545');
       const accounts = await web3.eth.getAccounts();
       const networkId = await web3.eth.net.getId();
       const contractAddress = artifact.networks[networkId].address;
 
-      // // instantiate contract instance and assign to component ref variable
       state.contract = new web3.eth.Contract(artifact.abi, contractAddress, {
         from: accounts[0],
       });
-
-      console.log('web3 data', accounts, networkId, contractAddress, state.contract)
     });
 
     const increment = async () => {
       await state.contract.methods.increment().send();
-      console.log('current counter', await state.contract.methods.getCount().call())
-    }
 
-    const getCount = async () => {
-      console.log(await state.contract.methods.getCount().call());
+      state.counter = await state.contract.methods.getCount().call();
     }
 
     return {
       state,
       increment,
-      getCount
     }
   },
 };
--- smart-contracts/contracts/Escrow.sol
+++ smart-contracts/contracts/Escrow.sol
@@ -4,7 +4,8 @@ pragma solidity >=0.4.22 <0.9.0;
 contract Escrow {
   address payable public buyer;
   address payable public seller;
-  address payable public arbiter;
+  address public arbiter;
+  mapping(address => uint256) TotalAmount;
 
   enum State {
     awate_payment,
@@ -14,7 +15,7 @@ contract Escrow {
 
   State public state;
 
-  modifier instante(State expected_state) {
+  modifier instate(State expected_state) {
     require(state == expected_state);
     _;
   }
@@ -22,5 +23,30 @@ contract Escrow {
   modifier onlyBuyer() {
     require(msg.sender == buyer || msg.sender == arbiter);
     _;
+  }
+
+  modifier onlySeller() {
+    require(msg.sender == seller);
+    _;
+  }
+
+  constructor(address payable _buyer, address payable _seller) public {
+    buyer = _buyer;
+    seller = _seller;
+    arbiter = msg.sender;
+  }
+
+  function confirmPayment() public onlyBuyer instate(State.awate_delivery) {
+    seller.transfer(address(this).balance);
+    state = State.awate_delivery;
+  }
+
+  function confirm_Delivery() public onlyBuyer instate(State.awate_delivery) {
+    seller.transfer(address(this).balance);
+    state = State.complete;
+  }
+
+  function ReturnPayment() public onlySeller instate(State.awate_delivery) {
+    buyer.transfer(address(this).balance);
   }
 }
